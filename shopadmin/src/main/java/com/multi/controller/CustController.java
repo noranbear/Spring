08e3401eb067fbefee055d2820572db657ad70de
noranbear <norandoly@gmail.com>
2022-06-09 10:22:33 +0900

    CustSelect로 데이터 가져와서 화면에 loop로 넣기 구현

--- shopadmin/src/main/java/com/multi/controller/CustController.java
+++ shopadmin/src/main/java/com/multi/controller/CustController.java
@@ -1,8 +1,14 @@
 package com.multi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.multi.biz.CustBiz;
+import com.multi.vo.CustVo;
 
 /**
  * @author noranbear
@@ -22,8 +28,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/cust")
 public class CustController {
 
+	@Autowired
+	CustBiz biz;
+	
 	@RequestMapping("/select")
 	public String select(Model m) {
+		List<CustVo> list = null;
+		try {
+			list = biz.get();
+			m.addAttribute("clist", list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		// center page 바꿔주기
 		m.addAttribute("center", "cust/select");
 		return "/index";
