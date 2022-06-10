d242a29c9501b794bf55d6454411f26c08083464
noranbear <norandoly@gmail.com>
2022-06-10 14:03:15 +0900

    Product select 및 add 페이지 구현

--- shopadmin/src/main/java/com/multi/controller/ProductController.java
+++ shopadmin/src/main/java/com/multi/controller/ProductController.java
@@ -1,5 +1,17 @@
 package com.multi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.multi.biz.CateBiz;
+import com.multi.biz.ProductBiz;
+import com.multi.vo.CateVo;
+import com.multi.vo.ProductVo;
+
 /**
  * @author noranbear
  * @date 2022. 6. 8.
@@ -14,8 +26,45 @@ package com.multi.controller;
  *
  * =========================================================
  */
+@Controller
+@RequestMapping("/product")
 public class ProductController {
 	
+	@Autowired
+	ProductBiz biz;
+	
+	@Autowired
+	CateBiz ctbiz;
+	
+	@RequestMapping("/add")
+	public String add(Model m) {
+		List<CateVo> list = null;
+		try {
+			list = ctbiz.get();
+			m.addAttribute("clist", list);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		m.addAttribute("center", "product/add");
+		return "/index";
+	}
+	
+	@RequestMapping("/select")
+	public String select(Model m) {
+		List<ProductVo> list = null;
+		
+		try {
+			list = biz.get();
+			m.addAttribute("plist", list);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		m.addAttribute("center", "product/select");
+		return "/index";
+	}
 	
 
 }
